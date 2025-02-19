6131a74534fd0b1ca3e605851210199595aa0f3e
tankankita <tankankita02@gmail.com>
2025-02-19 16:56:38 -0600

    add architecture

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import DroneSlateSimulator from "./simulators/DroneSlateSimulator.jsx";
+import DroneArchitectureSimulator from './simulators/DroneArchitectureSimulator.jsx'
 import DroneTransportationSimulator from "./simulators/DroneTransportationSimulator.jsx";
-import HomePage from "./home/dashboard/Homepage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<DroneTransportationSimulator />} />
         <Route path="/transportation" element={<DroneTransportationSimulator />} />
+        <Route path="/architecture" element={<DroneArchitectureSimulator />} />
+
+        
         <Route path="/slate" element={<DroneSlateSimulator />} />
 
       </Routes>
