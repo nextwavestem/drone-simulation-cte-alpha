6c760b615e24ffe7d058bd65eeca37d6c7f24041
haritaanbuvelan <haritavelan@gmail.com>
2025-03-03 13:03:19 -0600

    Changes to IT theme

--- src/environments/IT.jsx
+++ src/environments/IT.jsx
@@ -139,7 +139,7 @@ const displayCoordinatesText = (text, position) => {
 };
 
 const Model = () => {
-  const { scene } = useGLTF("assets/models/education/classroom2.glb");
+  const { scene } = useGLTF("assets/models/infotech/office.glb");
   const modelPosition = [255, -15, 500];
 
   // Set the desired rotation (in radians)
